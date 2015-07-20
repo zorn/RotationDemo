050addd88ce88f266af41bfabb74e589a2cb037d
Mike Zornek <mike@clickablebliss.com>
2015-07-20 16:59:03 -0700

    Moving current app to Swift folder and starting Objective-C version.

---
+++ Objective-C/RotationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RotationDemo
+//
+//  Created by Michael D Zornek on 7/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/RotationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RotationDemo
+//
+//  Created by Michael D Zornek on 7/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
